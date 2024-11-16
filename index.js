f220f8d338862e2433cb9f1f98b166c8a17bbe37
Jose Varela <joselcvarela@gmail.com>
2024-11-16 15:12:30 +0000

    Support multiple endpoints

--- index.js
+++ index.js
@@ -2,36 +2,55 @@ import axios from "axios";
 import { CronJob } from "cron";
 
 async function run() {
-  const url = new URL(process.env.ENDPOINT);
-  url.searchParams.set("cache-buster", Date.now());
+  try {
+    const url_list = process.env.ENDPOINT.split(";");
 
-  const online = await axios
-    .get(url.toString())
-    .then((r) => {
-      return r.status === 200;
-    })
-    .catch((r) => false);
+    const online = await Promise.all(
+      url_list.map(async (raw_url, i) => {
+        const url = new URL(raw_url);
+        url.searchParams.set("cache-buster", Date.now());
 
-  await axios.post(
-    "https://slack.com/api/chat.postMessage",
-    {
-      channel: process.env.SLACK_CHANNEL_ID,
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `${online ? "Online ✅" : "Offline ⛔️"} | Endpoint: ${url.toString()}`,
+        return await axios
+          .get(url.toString())
+          .then((r) => {
+            return r.status === 200;
+          })
+          .catch((r) => false);
+      })
+    );
+
+    await axios.post(
+      "https://slack.com/api/chat.postMessage",
+      {
+        channel: process.env.SLACK_CHANNEL_ID,
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: [
+                online.map((online) => (online ? "✅" : "⛔️")).join(" ") +
+                  "⤵️",
+
+                ...online.map(
+                  (online, i) =>
+                    `${online ? "Online ✅" : "Offline ⛔️"} | Endpoint: ${url_list[i]}`
+                ),
+              ].join("\n"),
+            },
           },
-        },
-      ],
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
+        ],
       },
-    }
-  );
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
+        },
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
 
 const job = new CronJob(process.env.CRON, run, null, true, "utc", null, true);
