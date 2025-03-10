bd128241e11f54d8c3ea2164cc60176b46b13c0d
Jose Varela <joselcvarela@gmail.com>
2025-03-10 07:03:55 +0000

    Add multiple attempts before alert

--- index.js
+++ index.js
@@ -3,7 +3,15 @@
 import axios from "axios";
 import { CronJob } from "cron";
 
-const { ENDPOINT, SLACK_CHANNEL_ID, SLACK_TOKEN, CRON, TIMEOUT } = process.env;
+const {
+  NODE_ENV,
+  RETRIES,
+  ENDPOINT,
+  SLACK_CHANNEL_ID,
+  SLACK_TOKEN,
+  CRON,
+  TIMEOUT,
+} = process.env;
 
 start();
 
@@ -20,6 +28,7 @@ async function run() {
   if (!SLACK_TOKEN) throw new Error("SLACK_TOKEN must be defined");
 
   const timeout = Number.isNaN(Number(TIMEOUT)) ? 4000 : Number(TIMEOUT);
+  const retries = Number.isNaN(Number(RETRIES)) ? 3 : Number(RETRIES);
 
   try {
     const urls = ENDPOINT.split(";").map((raw_url) => {
@@ -29,14 +38,7 @@ async function run() {
     });
 
     const status = await Promise.all(
-      urls.map(async (url, i) => {
-        return await axios
-          .get(url.toString())
-          .then((r) => {
-            return r.status === 200;
-          })
-          .catch((r) => false);
-      })
+      urls.map((url) => ping(url, { timeout, retries }))
     );
 
     const online = status.flatMap((online, i) =>
@@ -53,32 +55,61 @@ async function run() {
     if (offline.length) {
       console.log(offline.map((url) => `⛔ ${url}`));
 
-      await axios.post(
-        "https://slack.com/api/chat.postMessage",
-        {
-          channel: SLACK_CHANNEL_ID,
-          blocks: [
-            {
-              type: "section",
-              text: {
-                type: "mrkdwn",
-                text: [
-                  ...offline.map((url) => `"Offline ⛔️" | Endpoint: ${url}`),
-                ].join("\n"),
+      if (NODE_ENV !== "development") {
+        await axios.post(
+          "https://slack.com/api/chat.postMessage",
+          {
+            channel: SLACK_CHANNEL_ID,
+            blocks: [
+              {
+                type: "section",
+                text: {
+                  type: "mrkdwn",
+                  text: [
+                    ...offline.map((url) => `"Offline ⛔️" | Endpoint: ${url}`),
+                  ].join("\n"),
+                },
               },
-            },
-          ],
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${SLACK_TOKEN}`,
+            ],
           },
-          timeout,
-        }
-      );
+          {
+            headers: {
+              Authorization: `Bearer ${SLACK_TOKEN}`,
+            },
+          }
+        );
+      }
     }
   } catch (err) {
     console.error(err);
     throw err;
   }
+}
+
+/**
+ *
+ * @param {URL} url
+ * @param {object} [options]
+ * @param {number} [options.retries]
+ * @param {number} [options.timeout]
+ */
+async function ping(url, options, count = 0) {
+  count += 1;
+
+  if (options?.retries && count > options.retries) {
+    return false;
+  }
+
+  if (count > 1) {
+    await new Promise((res) => setTimeout(res, count * 2000));
+  }
+
+  const timeout = (options?.timeout || 1000) + count * 1000;
+
+  return await axios
+    .get(url.toString(), { timeout })
+    .then((r) => {
+      return r.status === 200;
+    })
+    .catch((r) => (options?.retries ? ping(url, options, count) : false));
 }
